48303d8abec9a3bd13176bc2aefe93cf34b22f6e
NishantTheProgrammer <knishant113@gmail.com>
2020-10-30 19:37:33 +0530

    Updates

---
+++ precache-manifest.90a3d09586644761d8718d976e36c053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e337900eac5a1fdcccb82c2b26a0921",
+    "url": "/AvatarMaker/index.html"
+  },
+  {
+    "revision": "b6439c2e64fce6bf50e0",
+    "url": "/AvatarMaker/static/css/main.50969194.chunk.css"
+  },
+  {
+    "revision": "312b427f7f5d60d1f617",
+    "url": "/AvatarMaker/static/js/2.7b358982.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/AvatarMaker/static/js/2.7b358982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6439c2e64fce6bf50e0",
+    "url": "/AvatarMaker/static/js/main.a3b8b833.chunk.js"
+  },
+  {
+    "revision": "10ecac8c78afc780dabf",
+    "url": "/AvatarMaker/static/js/runtime-main.418058dd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/AvatarMaker/static/media/logo.5d5d9eef.svg"
+  }
+]);
